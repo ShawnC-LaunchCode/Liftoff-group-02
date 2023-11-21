940fc9b6b4add502d1d28f82cc4460fb192c42b0
Riley <riley.neuville@gmail.com>
2023-11-20 20:40:07 -0600

    Init

---
+++ src/test/java/com/example/QSCLiftOff/QscLiftOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QSCLiftOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QscLiftOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
