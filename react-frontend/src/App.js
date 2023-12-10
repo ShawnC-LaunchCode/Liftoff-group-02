8a169ec23bef6b3bc0d18260d476115b2cef3c46
Riley <riley.neuville@gmail.com>
2023-12-10 17:36:47 -0600

    Added multiple pages and routing to react app

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,39 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import RegistrationPage from "./pages/registration";
+
 
 function App() {
-// Replace 'https://api.example.com' with the actual API endpoint
-const apiUrl = 'http://localhost:8080/api/users';
+return(
+<Router>
+    <Routes>
+        <Route exact path="/" element={<HomePage />}
+         />
 
-// Making a GET request
-axios.get(apiUrl)
-  .then(response => {
-    // Handle the successful response
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('Error:', error.message);
-  });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Route
+         path="/login"
+         element={<LoginPage />}
+         />
+
+         <Route
+          path="/register"
+          element={<RegistrationPage />}
+          />
+    </Routes>
+</Router>
+)
 }
-
 export default App;
