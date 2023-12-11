e3e16c9f6da174dfa8182b043b0cb7782ecee7e5
Jacob <jacobkellogg17@gmail.com>
2023-12-11 15:41:50 -0600

    started topbar and calendar preview for homepage

--- react-frontend/src/components/HomePageComponent.js
+++ react-frontend/src/components/HomePageComponent.js
@@ -1,22 +1,49 @@
 import React from 'react';
-// import {Container, Row, Col} from 'react-bootstrap'; 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../homepage.css';
 
 function HomePageComponent() {
     return (
-        <Container fluid ="xxl">
-          <Row>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
-          </Row>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-          </Row>
-        </Container>
-      );
+        <body className='HomePage'>
+           
+            <Container className='TopBar'>
+                <div className='Title'>My Calendar</div>
+            </Container>
+
+            <section className='CalendarPreview'>
+                <Container>
+                    <Row>
+                        <Col>1 of 2</Col>
+                        <Col>2 of 2</Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                        <Col>2 of 3</Col>
+                    </Row>
+                </Container>
+            </section>
+
+
+
+        </body>
+
+
+
+
+
+        // <Container>
+        //   <Row>
+        //     <Col>1 of 2</Col>
+        //     <Col>2 of 2</Col>
+        //   </Row>
+        //   <Row>
+        //     <Col>1 of 3</Col>
+        //     <Col>2 of 3</Col>
+        //   </Row>
+        // </Container>
+    );
 
 }
 
