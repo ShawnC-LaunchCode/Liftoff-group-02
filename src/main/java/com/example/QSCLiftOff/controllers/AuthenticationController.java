0190c00f3a837386aeeb8774c0ff392d711a1b58
Jacob <jacobkellogg17@gmail.com>
2024-01-06 19:41:36 -0600

    Begining the creation of sessions

--- src/main/java/com/example/QSCLiftOff/controllers/AuthenticationController.java
+++ src/main/java/com/example/QSCLiftOff/controllers/AuthenticationController.java
@@ -1,8 +1,12 @@
 package com.example.QSCLiftOff.controllers;
 
+import com.example.QSCLiftOff.models.User;
 import com.example.QSCLiftOff.models.data.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
 
 @Controller
 public class AuthenticationController {
@@ -10,5 +14,25 @@ public class AuthenticationController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final String userSessionKey = "user";
 
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    public User getUserFromSession(HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> userOpt = userRepository.findById(userId);
+
+        if (userOpt.isEmpty()){
+            return null;
+        }
+
+        return userOpt.get();
+    }
 }
