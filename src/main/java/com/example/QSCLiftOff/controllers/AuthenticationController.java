879b434b7cd1c6b66e738ca5fab6a55c01172021
Jacob <jacobkellogg17@gmail.com>
2024-01-08 18:51:05 -0600

    Added dependecies

--- src/main/java/com/example/QSCLiftOff/controllers/AuthenticationController.java
+++ src/main/java/com/example/QSCLiftOff/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.example.QSCLiftOff.models.data.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Optional;
 
