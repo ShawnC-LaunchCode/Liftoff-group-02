abcbfe2f800bb037f2fcf6e62b9ec3abd2aabb83
Jacob <jacobkellogg17@gmail.com>
2024-01-10 21:35:30 -0600

    addded with credentials to POST method in handleAddEvent

--- react-frontend/src/components/Modal.js
+++ react-frontend/src/components/Modal.js
@@ -29,7 +29,7 @@ const Modal = ({ isOpen, onClose }) => {
 
         const handleAddEvent = async() => {
           try {
-            const response = await axios.post('http://localhost:8080/api/createEvent', newEvent);
+            const response = await axios.post('http://localhost:8080/api/createEvent', newEvent, {withCredentials: true});
             console.log(response.data);  // Handle success response
             
           } catch (error) {
