c77f1299f7f9e27d652eb16f3d7551bf2094d254
Jacob <jacobkellogg17@gmail.com>
2024-01-11 15:44:04 -0600

    spring security is now connected to database. Can login using stored users

--- src/main/java/com/example/QSCLiftOff/config/SecurityConfig.java
+++ src/main/java/com/example/QSCLiftOff/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.example.QSCLiftOff.config;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 @Configuration
@@ -27,19 +32,22 @@ public class SecurityConfig {
     @Value("${spring.websecurity.debug:false}")
     boolean webSecurityDebug;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.debug(webSecurityDebug);
     }
 
-    @Bean
-    InMemoryUserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("jacob")
-                        .password("{noop}1234")
-                        .roles("ADMIN")
-                        .build()
-        );
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, pwhash, id FROM liftoff.user WHERE username=?")
+                .authoritiesByUsernameQuery("select username, id FROM liftoff.user WHERE username=?");
     }
 
     @Bean
@@ -52,8 +60,11 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-//                .formLogin(Customizer.withDefaults());
-                .formLogin(formLogin -> formLogin.defaultSuccessUrl("http://localhost:3000"));
+//                .formLogin(formLogin -> formLogin.loginPage("http://localhost:3000/login"))
+                .formLogin(formLogin -> formLogin.permitAll())
+                .logout(logout -> logout.permitAll());
+
+
 
         return http.build();
     }
